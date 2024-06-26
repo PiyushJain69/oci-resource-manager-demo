86d0465f04b936a44b3a9a7bf76102353ea8df92
PiyushJain69 <54004086+PiyushJain69@users.noreply.github.com>
2024-06-26 12:41:18 +0100

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,7 @@
+provider "oci" {
+  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaawfssm6kwqr5452uyvokkboixuyl7cgrb5zqp4qzor4iddflkgpja"
+  user_ocid = "ocid1.user.oc1..aaaaaaaaiopwdnjxvyuh5lxqpn6rx2d3aehlibh7l3wqmqbxedrfkxnucadq" 
+  private_key_path = "/home/opc/.oci/DevOps.pem"
+  fingerprint = "98:75:46:c8:26:9b:80:6a:4f:87:9d:e5:a3:ad:3b:82"
+  region = "ap-sydney-1"
+}
